0f7e3cdadc9489cde469d982e742b1ad36fb2af4
nick <r951236958@gmail.com>
2020-10-23 18:51:22 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+//  
+//  function MyApp({ Component, pageProps }) {
+//    return <Component {...pageProps} />
+//  }
+//  
+//  export default MyApp
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
